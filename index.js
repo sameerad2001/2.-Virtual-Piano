21d228e1cd4dc7dcd3d3772551e04605df88eaf4
Sameer <sameerad2001@gmail.com>
2021-06-20 15:56:15 +0530

    Added Button Animation

--- index.js
+++ index.js
@@ -3,47 +3,59 @@ document.addEventListener('keydown', (event) => {
     switch (event.key) {
         // Normal Keys
         case 'a':
+            animate(0, false)
             play_sound('A', false)
             break;
 
         case 's':
+            animate(1, false)
             play_sound('B', false)
             break;
 
         case 'd':
+            animate(2, false)
             play_sound('C', false)
             break;
 
         case 'f':
+            animate(3, false)
             play_sound('D', false)
             break;
 
         case 'g':
+            animate(4, false)
             play_sound('E', false)
             break;
 
         case 'h':
+            animate(5, false)
             play_sound('F', false)
             break;
 
         case 'j':
+            animate(6, false)
             play_sound('G', false)
             break;
 
             // Sharp Keys
         case 'w':
+            animate(0, true)
             play_sound('A', true)
             break;
         case 'e':
+            animate(1, true)
             play_sound('C', true)
             break;
         case 'r':
+            animate(2, true)
             play_sound('D', true)
             break;
         case 't':
+            animate(3, true)
             play_sound('F', true)
             break;
         case 'y':
+            animate(4, true)
             play_sound('G', true)
             break;
     }
@@ -65,7 +77,7 @@ let piano_tiles = document.getElementsByClassName('piano_tiles')
 
 for (let i = 0; i < piano_tiles.length; i++) {
     piano_tiles[i].addEventListener('click', () => {
-        console.log(this.innerHTML)
+        animate(i, false);
         play_sound(piano_tiles[i].innerHTML, false)
     });
 }
@@ -75,8 +87,27 @@ let piano_tiles_sharp = document.getElementsByClassName('piano_tiles_sharp')
 
 for (let i = 0; i < piano_tiles_sharp.length; i++) {
     piano_tiles_sharp[i].addEventListener('click', () => {
+        animate(i, true);
         play_sound(piano_tiles_sharp[i].innerHTML[0], true)
     });
+}
+
+
+// Animate on Click 
+function animate(index, sharp) {
+    if (!sharp) {
+        piano_tiles[index].classList.add('flash');
+
+        setTimeout(() => {
+            piano_tiles[index].classList.remove('flash');
+        }, 100)
+    } else {
+        piano_tiles_sharp[index].classList.add('flash');
+
+        setTimeout(() => {
+            piano_tiles_sharp[index].classList.remove('flash');
+        }, 100)
+    }
 }
 
 // ____________________________________________________________Footer Dynamic Date
